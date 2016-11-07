6dfb955c1d777fb267bdc177dbb61a9479036c10
Jacqueline Luo <luo.jackie@hotmail.com>
2016-11-07 13:43:43 -0500

    New code

--- Lab_4/Part 3/test.cpp
+++ Lab_4/Part 3/test.cpp
@@ -2,35 +2,18 @@
 #define F_CPU 10000000  // system clock is 20 MHz
 #include <util/delay.h>  // uses F_CPU to achieve us and ms delays
  
-// Motor Control Functions -- pwm is an 8-bit value
-//  (i.e. ranges from 0 to 255)
- 
-void M1_forward(unsigned char pwm)
-{
-  OCR0A = 0;
-  OCR0B = pwm;
-}
- 
-void M1_reverse(unsigned char pwm)
-{
-  OCR0B = 0;
-  OCR0A = pwm;
-}
- 
-void M2_forward(unsigned char pwm)
-{
-  OCR2A = 0;
-  OCR2B = pwm;
-}
- 
-void M2_reverse(unsigned char pwm)
-{
-  OCR2B = 0;
-  OCR2A = pwm;
-}
-
+//Functions that control the car. 
+//Fnt = front wheels, Bak = back wheels
+//fwd = forward, rev = reverse
 void fwdFnt()
 {
+	PORTB |= 0b00100000;
+	PORTD |= 0b10000000;
+}
+
+void fwdFnt(unsigned char pwm)
+{
+	OCR0A = OCR0B = pwm;
 	PORTB |= 0b00100000;
 	PORTD |= 0b10000000;
 }
@@ -41,10 +24,11 @@ void revFnt()
 	PORTD |= 0b00000000;
 }
 
- void revBak()
-{
-	PORTB |= 0b00000100;
-	PORTD |= 0b00000100;
+void revFnt(unsigned char pwm)
+{	
+	OCR0A = OCR0B = pwm;
+	PORTB |= 0b00010001;
+	PORTD |= 0b00000000;
 }
 
 void fwdBak()
@@ -52,6 +36,26 @@ void fwdBak()
 	PORTB |= 0b00000010;
 	PORTD |= 0b00010000;
 } 
+
+void fwdBak(unsigned char pwm)
+{
+	OCR2A = OCR2B = pwm;
+	PORTB |= 0b00000010;
+	PORTD |= 0b00010000;
+} 
+
+ void revBak()
+{
+	PORTB |= 0b00000100;
+	PORTD |= 0b00000100;
+}
+
+ void revBak(unsigned char pwm)
+{
+	OCR2A = OCR2B = pwm;
+	PORTB |= 0b00000100;
+	PORTD |= 0b00000100;
+}
 
 void revAll(unsigned char pwm)
 {
@@ -88,19 +92,19 @@ void motors_init()
     //  Timer0 and Timer2 count up from 0 to 255
     TCCR0A = TCCR2A = 0xF3;
  
-    // use the system clock/8 (=2.5 MHz) as the timer clock
+    //prescaler /1028
     TCCR0B = TCCR2B = 0x05; //start the clock
  
-    // initialize all PWMs to 0% duty cycle (braking)
+    // start clocks
     OCR0A = OCR0B = OCR2A = OCR2B = 0;
  
-    // set PWM pins as digital outputs (the PWM signals will not
-    // appear on the lines if they are digital inputs)
+	// set pins as output
 	DDRD |= (1 << PD7) | (1 << PD4) | (1 << PD2) | (1 << PD3) | (1 << PD6) | (1 << PD0) | (1 << PD5);
 	DDRB |= (1<< PB0) |(1 << PB1) | (1 << PB2) | (1 << PB3) | (1 << PB4) | (1 << PB5);
 }
 
- 
+
+//I took this code from online. 
 // delay for time_ms milliseconds by looping
 //  time_ms is a two-byte value that can range from 0 - 65535
 //  a value of 65535 (0xFF) produces an infinite delay
@@ -122,20 +126,14 @@ int main()
     motors_init();
 	
 	//fwdFnt();
-	fwdAll(100); //the smaller the nb the faster the speed.
-	
-    //M1_forward(255);  // motor 1 forward at half speed
-   // M2_reverse(25);  // motor 2 reverse at 10% speed
- 
-    //delay_ms(5000);  // delay for 2s while motors run
- 
-    //M1_reverse(255);  // motor 1 reverse at 25% speed
-    //M2_forward(0);  // motor 2 stop/brake
- 
-    // loop here forever to keep the program counter from
-    //  running off the end of our program
+	//fwdFnt(100); //the smaller the nb the faster the speed. 200 seems to be too weak
+	fwdFnt(100);
+	revBak(100);
+   
     while (1)
-        ;
- 
+	{
+		
+	}
+        
     return 0;
 }
